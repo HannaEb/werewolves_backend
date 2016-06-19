8f9f714210a2095f9b33a1febd64893d0743f743
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-06-19 14:41:37 +0100

    bio and username added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,42 @@ class ApplicationController < ActionController::API
   # skip_before_filter  :verify_authenticity_token
   #
   # protect_from_forgery with: :null_session
+  #
+  # devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:nickname, :email, :password) }
+  #
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :bio])
+  end
 end
+
+
+# def create
+#   user = User.create sign_up_params
+#   user.save
+#   render :json => user
+# end
+#
+#
+#   private
+#
+#    def sign_up_params
+#      params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation)
+#    end
+#
+#    def account_update_params
+#      params.require(:user).permit(:name, :nickname, :email, :password, :password_confirmation, :current_password)
+#    end
+# private
+#
+#   def sign_up_params
+#     params.require(:user).permit(:first_name, :last_name, :nickname, :email, :password, :password_confirmation)
+#   end
+#
+#   def account_update_params
+#     params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+#   end
