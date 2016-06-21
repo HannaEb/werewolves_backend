7aa50a6dedce33294b7c9678d9262c1d8a3d4f7a
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-06-21 18:57:15 +0100

    heroku upload

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,13 @@ class User < ActiveRecord::Base
   include DeviseTokenAuth::Concerns::User
 
 
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :bio])
+  end
 
   has_one :profile
 end
