98bb0aa166f96df5aa8a207467ffe4352c377ce5
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-06-21 19:04:32 +0100

    heroku upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,18 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+
+
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+    protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :bio, :image])
+    end
+
+
+
   #  before_filter :add_allow_credentials_headers
   #  skip_before_filter :verify_authenticity_token
   #  before_filter :cors_preflight_check
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,14 +6,7 @@ class User < ActiveRecord::Base
 
   include DeviseTokenAuth::Concerns::User
 
-  before_action :configure_permitted_parameters, if: :devise_controller?
+
 
   has_one :profile
-
-  protected
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :bio])
-  end
-
 end
