383373a4b29d97a880f7d4373f17ba3c5be2df47
Harry Wynn-Williams <harrywynnwilliams@googlemail.com>
2016-06-22 16:57:18 +0100

    heroku

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
-  Include default devise modules.
-  devise :database_authenticatable,
-  :registerable,
+  # Include default devise modules.
+  devise :database_authenticatable,:registerable,
           :recoverable, :rememberable, :trackable, :validatable,
           :omniauthable
 
